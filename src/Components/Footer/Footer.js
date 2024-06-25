47f13bc61781c3911976bc88c9f2f5564aa28833
joannasp12 <jhpoon@usc.edu>
2024-06-25 16:36:20 -0700

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,52 +1,56 @@
-import React, { useState } from 'react';
-import './Footer.css';
-import Link from '../Link/Link.js';
-
+import React, { useState } from "react";
+import "./Footer.css";
+import Link from "../Link/Link.js";
 
 function Footer() {
-    const [selectedLink, setSelectedLink] = useState('Work'); // Start with 'Work' selected
+  const [selectedLink, setSelectedLink] = useState("Work"); // Start with 'Work' selected
 
-    const handleLinkClick = (text) => {
-        if(text !=='Resume'){
-            setSelectedLink(text);
-        }
-        
-    };
-    const handleEmailClick = () => {
-        window.location.href = 'mailto:joannaxpoon@gmail.com';
-      };
-    
+  const handleLinkClick = (text) => {
+    if (text !== "Resume") {
+      setSelectedLink(text);
+    }
+  };
+  const handleEmailClick = () => {
+    window.location.href = "mailto:joannaxpoon@usc.edu";
+  };
 
-    return (
-        <div className="Footer">
-            <div className="LeftLinks">
-            <div>
-            <p>Coded with love and care (+ React, Js, etc.) by Joanna Poon (Yours Truly)</p>
-            
-            </div>
-            </div>
-            <div className="Right">
-                <div className="RightLinks">
-                <Link
-                    href="https://drive.google.com/file/d/10Ew9k9THc-U8e789pCuLnDpaSmPO8K65/view?usp=sharing"
-                    text="Resume"
-                    isArrow ="true"
-                    isSelected={selectedLink === 'Resume'}
-                    onClick={() => handleLinkClick('Resume')}
-                />
-                <Link
-                    href="https://www.linkedin.com/in/joannapoon-/"
-                    text="LinkedIn"
-                    isArrow ="true"
-                    isSelected={selectedLink === 'Resume'}
-                    onClick={() => handleLinkClick('Resume')}
-                />
-                </div>
-                <button onClick={handleEmailClick} className="emailButton">joannaxpoon@usc.edu</button>
-
-            </div>
+  return (
+    <div className="Footer">
+      <div className="LeftLinks">
+        <div>
+          <p>
+            Coded with love and care (+ React, Js, etc.) by Joanna Poon (Yours
+            Truly)
+          </p>
         </div>
-    );
+      </div>
+      <div className="Right">
+        <div className="RightLinks">
+          <Link
+            href="https://drive.google.com/file/d/10Ew9k9THc-U8e789pCuLnDpaSmPO8K65/view?usp=sharing"
+            text="Resume"
+            isArrow="true"
+            isSelected={selectedLink === "Resume"}
+            onClick={() => handleLinkClick("Resume")}
+          />
+          <Link
+            href="https://www.linkedin.com/in/joannapoon-/"
+            text="LinkedIn"
+            isArrow="true"
+            isSelected={selectedLink === "Resume"}
+            onClick={() => handleLinkClick("Resume")}
+          />
+        </div>
+        <a href="mailto:joannaxpoon@gmail.com">
+        <button className="emailButton">
+          joannaxpoon@gmail.com
+        </button>
+        </a>
+
+       
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
