a14a5e38393ed4f16dc05c4eb3017fa82282b3b9
joannasp12 <jhpoon@usc.edu>
2024-06-28 16:27:50 -0700

    change image

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import arrow from "./../Assets/Arrow.svg";
-import me from "./../Assets/me.jpg";
+import me from "./../Assets/me.png";
 import { useNavigate } from "react-router-dom";
 
 
